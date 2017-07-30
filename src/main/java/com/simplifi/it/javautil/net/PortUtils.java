1db974d74480a0947abe008f06e3e6177cfec296
Timothy Farkas <tim@simplifi.it>
2017-07-29 19:44:58 -0700

    - Added port constants

---
+++ src/main/java/com/simplifi/it/javautil/net/PortUtils.java
@@ -0,0 +1,8 @@
+package com.simplifi.it.javautil.net;
+
+public class PortUtils
+{
+  public static final Port HTTP_PORT = new Port(80);
+  public static final Port HTTPS_PORT = new Port(443);
+  public static final Port SSH_PORT = new Port(22);
+}
