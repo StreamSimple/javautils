5c2fdb34914d622437cc97127e8dda9d24891b6f
Timothy Farkas <tim@simplifi.it>
2017-11-15 23:18:23 -0800

    - Added Host class

--- src/main/java/com/simplifi/it/javautil/net/PortUtils.java
+++ src/main/java/com/simplifi/it/javautil/net/PortUtils.java
@@ -2,7 +2,11 @@ package com.simplifi.it.javautil.net;
 
 public class PortUtils
 {
-  public static final Port HTTP_PORT = new Port(80);
-  public static final Port HTTPS_PORT = new Port(443);
-  public static final Port SSH_PORT = new Port(22);
+  public static final int DEFAULT_HTTP_PORT = 80;
+  public static final int DEFAULT_HTTPS_PORT = 443;
+  public static final int DEFAULT_SSH_PORT = 22;
+
+  public static final Port HTTP_PORT = new Port(DEFAULT_HTTP_PORT);
+  public static final Port HTTPS_PORT = new Port(DEFAULT_HTTPS_PORT);
+  public static final Port SSH_PORT = new Port(DEFAULT_SSH_PORT);
 }
