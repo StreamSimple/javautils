16077dadb4e6505988b9b2d74112064899442750
Timothy Farkas <tim@simplifi.it>
2017-11-26 22:08:26 -0800

    - Added a string serde test

---
+++ src/test/java/com/simplifi/it/javautil/serde/StringSerdeTest.java
@@ -0,0 +1,26 @@
+package com.simplifi.it.javautil.serde;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.nio.charset.StandardCharsets;
+
+public class StringSerdeTest
+{
+  @Test
+  public void simpleSerdeTest()
+  {
+    final StringSerializer serializer = new StringSerializer.Builder()
+        .setCharsetName(StandardCharsets.US_ASCII.name())
+        .build();
+
+    final StringDeserializer deserializer = new StringDeserializer.Builder()
+        .setCharsetName(StandardCharsets.US_ASCII.name())
+        .build();
+
+    final String expected = "I'm flying";
+    final byte[] bytes = serializer.serialize(expected);
+    final String actual = deserializer.deserialize(bytes);
+
+    Assert.assertEquals(expected, actual);
+  }
+}
