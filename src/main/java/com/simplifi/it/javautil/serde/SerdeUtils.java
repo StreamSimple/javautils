73db369f181e3bf8c430c283be7c04c1410a02df
Timothy Farkas <tim@simplifi.it>
2017-11-29 22:31:35 -0800

    - Added some more serialization methods

--- src/main/java/com/simplifi/it/javautil/serde/SerdeUtils.java
+++ src/main/java/com/simplifi/it/javautil/serde/SerdeUtils.java
@@ -5,7 +5,7 @@ public class SerdeUtils
   public static int NUM_BYTES_LONG = 8;
 
   /**
-   * Serializes the given long value to an array of bytes.
+   * Serializes the given long value to an array of bytes. BigEndian.
    * @param val The long value to serialize.
    * @return The serialized long value.
    */
@@ -25,19 +25,25 @@ public class SerdeUtils
     return buffer;
   }
 
+  public static void serializeLong(long val, int offset, byte[] buffer)
+  {
+    buffer[0 + offset] = (byte)((val >> 56) & 0xFFL);
+    buffer[1 + offset] = (byte)((val >> 48) & 0xFFL);
+    buffer[2 + offset] = (byte)((val >> 40) & 0xFFL);
+    buffer[3 + offset] = (byte)((val >> 32) & 0xFFL);
+    buffer[4 + offset] = (byte)((val >> 24) & 0xFFL);
+    buffer[5 + offset] = (byte)((val >> 16) & 0xFFL);
+    buffer[6 + offset] = (byte)((val >> 8) & 0xFFL);
+    buffer[7 + offset] = (byte)(val & 0xFFL);
+  }
+
   /**
-   * Deserializes the given long value.
+   * Deserializes the given BigEndian long value.
    * @param buffer A serialized long value.
    * @return The deserialized long value.
    */
   public static long deserializeLong(byte[] buffer)
   {
-    if (buffer.length != NUM_BYTES_LONG) {
-      final String message = String.format("The provided buffer had length %s, but expected it to have length %s",
-          buffer.length, NUM_BYTES_LONG);
-      throw new IllegalArgumentException(message);
-    }
-
     return (((long)buffer[0]) & 0xFFL) << 56 |
         ((((long)buffer[1]) & 0xFFL) << 48) |
         ((((long)buffer[2]) & 0xFFL) << 40) |
