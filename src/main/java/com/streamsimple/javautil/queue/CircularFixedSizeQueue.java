22b6f31b70a11337b4de8c63411365bf5ae12d67
Timothy Farkas <tim@streamsimple.io>
2019-02-26 23:49:17 -0800

    - Added initial code for circular fixed size queue.

---
+++ src/main/java/com/streamsimple/javautil/queue/CircularFixedSizeQueue.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsimple.javautil.queue;
+
+public class CircularFixedSizeQueue<E>
+{
+  private final E[] array;
+  private int start = 0;
+  private int end = 0;
+
+  public CircularFixedSizeQueue(int maxSize)
+  {
+    this.array = (E[])new Object[maxSize];
+  }
+
+  public void clear()
+  {
+    start = 0;
+    end = 0;
+
+    for (int i = 0; i < array.length; i++) {
+      array[i] = null;
+    }
+  }
+
+  public void offer(E element)
+  {
+  }
+
+  public E poll()
+  {
+    return null;
+  }
+
+  public E peek()
+  {
+    return null;
+  }
+
+  public static int circularMod(int val, int mod)
+  {
+    int result = val % mod;
+
+    if (result < 0) {
+      return result + mod;
+    } else {
+      return result;
+    }
+  }
+}
